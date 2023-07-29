fccee46d99ed6224912d75bfa8a81d0032c6e113
Khan Muhammad Hridoy <khanmuhammadhridoy@gmail.com>
2023-07-29 18:46:58 +0600

    New branch

---
+++ sonar.ino
@@ -0,0 +1,51 @@
+// defines pins numbers
+#define LED 5
+int trigPin = 2;
+int echoPin = 4;
+// defines variables
+long duration;
+int distance;
+void setup()
+{
+    pinMode(5, OUTPUT);
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+    Serial.begin(36000);      // Starts the serial communication
+}
+void loop()
+{
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    distance = duration * 0.034 / 2;
+    // Prints the distance on the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    // check distance and blinking
+    if ((distance >= 0) && (distance <= 100))
+    {
+        digitalWrite(LED, HIGH);
+    }
+    else
+    {
+        digitalWrite(LED, LOW);
+    }
+    if ((distance >= 100) && (distance <= 200))
+    {
+        digitalWrite(LED, HIGH);
+        delay(500);
+        digitalWrite(LED, LOW);
+        delay(500);
+    }
+    else
+    {
+        digitalWrite(LED, LOW);
+    }
+}
